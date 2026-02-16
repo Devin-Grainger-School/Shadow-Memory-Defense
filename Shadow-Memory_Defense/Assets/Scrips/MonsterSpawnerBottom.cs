885189b4e8f510e9bde938cdd8d147793d644727
Devin-Grainger-School <dgraing1@student.fitchburgstate.edu>
2026-02-16 18:48:31 -0500

    Bottom Spawn Mnaager

---
+++ Shadow-Memory_Defense/Assets/Scrips/MonsterSpawnerBottom.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MonsterSpawnerBottom : MonoBehaviour
+{
+    public GameObject[] MonsterPrefabs;
+    private float spawnRangeX = 10;
+    private float spawnRangeY = 10;
+    private float spawnRangeZ = -.4f;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       InvokeRepeating("SpawnRandomMonster", startDelay, spawnInterval); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), -15, spawnRangeZ);
+        int monsterIndex = Random.Range(0, MonsterPrefabs.Length);
+        Instantiate(MonsterPrefabs[monsterIndex], spawnPos, MonsterPrefabs[monsterIndex].transform.rotation);
+    }
+}
